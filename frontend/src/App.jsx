b3c0a6ae44cf8ab642b1c6c22cc9afbcd5624ca2
Parthav <parthavsabrwal@gmail.com>
2025-02-19 03:19:08 +0530

    Latest code pushed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MapPage from './pages/MapPage';
 import LoginRegister from './pages/LoginRegister';
+import FeedbackPage from './pages/FeedbackPage';
+import ContactusPage from './pages/ContactusPage';
 
 function App() {
   return (
@@ -10,9 +12,12 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/map" element={<MapPage />} />
         <Route path="/login-register" element={<LoginRegister />} />
+        <Route path="/feedback" element={<FeedbackPage />} />
+        <Route path="/contact-us" element={<ContactusPage />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
